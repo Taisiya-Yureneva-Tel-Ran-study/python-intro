54eace4b55a2a234dbb010e709e8035437f37f4a
t.yureneva <taisiya.yureneva@gmail.com>
2025-09-20 17:15:40 +0300

    HW #21 part 1 refactored

---
+++ README.md
@@ -0,0 +1,15 @@
+# Python intro
+
+## HW #21
+
+### Function bSearchInSortedList
+1. In the case there are several occurrences of a being searched number, the function
+returns the index of the first occurrence
+
+    a. Keep O[LogN] complexity
+
+    b. Example: numbers = [20, 20, 20, 20, 20],
+
+        - bSearchInSortedList(numbers, 20)
+    
+        - returns 0
--- main.py
+++ main.py
@@ -2,18 +2,18 @@ def bSearchInSortedList(lst: list[int], num: int) -> int:
     left: int = 0
     right: int = len(lst) - 1
     mid: int = (left + right) // 2
-    while left <= right:
+    found: int = -1
+    while left <= right and found == -1:
         print(left, right, mid)
         if lst[mid] == num:
-            ar = lst[left:mid]
-            res = bSearchInSortedList(ar, num)
-            return mid if res == -1 else res + left
+            res = bSearchInSortedList(lst[left:mid], num)
+            found = mid if res == -1 else res + left
         elif lst[mid] < num:
             left = mid + 1
         else:
             right = mid - 1
         mid = (left + right) // 2
-    return -1
+    return found
 
-numbers: list[int] = [1, 2, 3, 4, 5, 5, 5, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]
+numbers: list[int] = [1, 5, 15, 15, 15, 16, 17, 18, 19]
 print(bSearchInSortedList(numbers, 15))
